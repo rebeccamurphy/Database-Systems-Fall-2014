47ea0e275e5b2ee41dc83d0a6639b96f4827ee23
rebeccamurphy <rebeccalorenmurphy@gmail.com>
2014-12-05 02:58:10 -0500

    up to triggers

--- DesignProject/comedians.sql
+++ DesignProject/comedians.sql
@@ -1,3 +1,11 @@
+
+DROP TABLE IF EXISTS Comedians, Troupes, BelongsTo, IsIn, Media, Audio, Visual, AudioVisual, RadioSeries,
+Albums, Books, TVShows,Movies, StandUpShows;
+
+DROP TYPE IF EXISTS GENDER ;
+
+--create table statements--
+
 CREATE TYPE GENDER AS ENUM ('Female', 'Male', 'Other');
 CREATE TABLE IF NOT EXISTS Comedians (
 	 CID		 	 CHAR(8)	 	 NOT NULL,
@@ -21,7 +29,7 @@ VALUES
 ('C0000007', 'Kate Micucci', date '3/31/1980', 'Female', 'American', TRUE, 2001, NULL ),
 ('C0000008', 'Riki Lindhome', date '3/5/1979', 'Female', 'American', TRUE, 2001, NULL ),
 ('C0000009', 'Dara Ó Briain', date '2/4/1972', 'Male', 'Irish', TRUE, 1998, NULL ),
-('C0000010', 'Dylan Moran', date '11/3/1971', 'Male', 'Irish', TRUE, 1992, NULL )
+('C0000010', 'Dylan Moran', date '11/3/1971', 'Male', 'Irish', TRUE, 1992, NULL ),
 ('C0000011', 'Julian Barratt', date '5/4/1968', 'Male', 'British', TRUE, 1995, NULL ),
 ('C0000012', 'Noel Fielding', date '5/21/1973', 'Male', 'British', TRUE, 1996, NULL );
 
@@ -164,16 +172,6 @@ CREATE TABLE IF NOT EXISTS Books (
 INSERT INTO Books VALUES
 ('B0001','Visual001', 64);
 
-CREATE TABLE IF NOT EXISTS TVShows (
-	 TVID		 		CHAR(5)	    NOT NULL,
-	 AudioVisualID			CHAR(8)	    NOT NULL REFERENCES AudioVisual(AudioVisualID),
-	 YearEnded			INT	    NOT NULL,
-	 NumSeasons			INT	    NOT NULL,
-	 NumEpisodes			INT	    NOT NULL,
-	 EpisodeLengthMins		INT	    NOT NULL, 
-	 TVStation			TEXT	    NOT NULL,
-	 PRIMARY KEY (TVID)
-);
 CREATE TABLE IF NOT EXISTS AudioVisual (
 	 AudioVisualID 	CHAR(15)	    NOT NULL,
 	 MediaID	CHAR(10)	    NOT NULL REFERENCES Media(MediaID),
@@ -187,8 +185,8 @@ INSERT INTO AudioVisual VALUES
 ('AudioVisual0004','Media00005');
 
 CREATE TABLE IF NOT EXISTS TVShows (
- TVID		 		CHAR(5)	    NOT NULL,
- AudioVisualID		CHAR(8)	    NOT NULL REFERENCES AudioVisual(AudioVisualID),
+ TVID		 		CHAR(6)	    NOT NULL,
+ AudioVisualID		CHAR(15)    NOT NULL REFERENCES AudioVisual(AudioVisualID),
  YearEnded			INT		    NOT NULL,
  NumSeasons			INT		    NOT NULL,
  NumEpisodes		INT		    NOT NULL,
@@ -222,3 +220,140 @@ CREATE TABLE IF NOT EXISTS StandUpShows (
 
 INSERT INTO StandUpShows VALUES
 ('SU0001', 'AudioVisual0004', 102, 'London', 'England');
+
+
+--create views statements --
+DROP VIEW IF EXISTS TroupesMembers;
+CREATE VIEW TroupesMembers
+AS
+SELECT DISTINCT t.name as "Troupe", c.name as "Comedian"
+FROM Troupes as t, Comedians as c, BelongsTo as b
+WHERE b.TID = t.TID AND b.CID = c.CID
+ORDER BY t.name;
+
+DROP VIEW IF EXISTS ComediansMedia;
+CREATE VIEW ComediansMedia
+AS
+SELECT DISTINCT c.name as "Comedian", m.name as "Media"
+FROM Media as m, Comedians as c, IsIn as relate
+WHERE relate.MediaID = m.MediaID AND relate.CID = c.CID
+ORDER BY c.name;
+
+DROP VIEW IF EXISTS TroupesMedia;
+CREATE VIEW TroupesMedia AS
+SELECT DISTINCT t.name, mediaid
+FROM BelongsTo AS b
+join IsIn AS relate
+ON 
+b.CID = relate.CID
+join Troupes AS t
+ON
+b.TID = t.TID
+ORDER BY t.name;
+
+
+--stored procedures--
+DROP FUNCTION IF EXISTS GetTroupes(cname text);
+CREATE OR REPLACE FUNCTION GetTroupes(cname text)
+RETURNS SETOF TEXT AS $$
+BEGIN
+RETURN QUERY 
+SELECT t.name
+from Troupes as t 
+where t.tid in 
+	(select TID
+	from BelongsTo as relate 
+	join Comedians as c
+	on relate.CID = c.CID and c.name = cname
+	)
+order by t.name;
+END;
+$$ LANGUAGE plpgsql;
+
+DROP FUNCTION IF EXISTS GetTroupeMembers(tname text);
+CREATE OR REPLACE FUNCTION GetTroupeMembers(tname text)
+RETURNS SETOF TEXT AS $$
+BEGIN
+RETURN QUERY 
+SELECT c.name 
+from Comedians as c 
+where c.cid in 
+	(select CID
+	from BelongsTo as relate 
+	join Troupes as t
+	on relate.TID = t.TID and t.name = tname
+	)
+order by c.name;
+END;
+$$ LANGUAGE plpgsql;
+
+DROP FUNCTION IF EXISTS GetComedianMedia(cname text);
+CREATE OR REPLACE FUNCTION GetComedianMedia(cname text)
+RETURNS SETOF TEXT AS $$
+BEGIN
+RETURN QUERY 
+SELECT m.name 
+from Media as m
+where m.MediaID in 
+	(select MediaID
+	from IsIn as relate 
+	join Comedians as c
+	on relate.CID = c.CID and c.name = cname
+	)
+order by m.name;
+END;
+$$ LANGUAGE plpgsql;
+
+DROP FUNCTION IF EXISTS GetTroupeMedia(tname text);
+CREATE OR REPLACE FUNCTION GetTroupeMedia(tname text)
+RETURNS SETOF TEXT AS $$
+BEGIN
+RETURN QUERY 
+SELECT m.name 
+from Media as m
+where m.MediaID in 
+	(select MediaID
+	from IsIn as relate 
+	join Comedians as c
+	on relate.CID = c.CID
+	join BelongsTo as b
+	on b.CID = c.CID
+	join Troupes as t
+	on b.TID = t.TID and tname = t.name
+	)
+order by m.name;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE ROLE Admin
+GRANT SELECT, INSERT, UPDATE, ALTER
+ON ALL TABLES IN SCHEMA PUBLIC
+TO Admin;
+
+
+CREATE ROLE User
+GRANT SELECT
+ON ALL TABLES IN SCHEMA PUBLIC
+TO User;
+
+drop trigger if exists  check_year_listener on Troupes;
+drop function if exists checkYear();
+CREATE FUNCTION checkYear() RETURNS trigger AS $$
+    BEGIN
+        -- Check that empname and salary are given
+        IF (TG_TABLE_NAME= 'Comedians') THEN
+	        IF (NEW.YearFirstActive <0) THEN
+	            RAISE EXCEPTION 'Years cannot be negative cannot be null';
+	        END IF;
+	    ENDIF;
+	    IF (TG_TABLE_NAME= 'Troupes') THEN
+	        IF (NEW.YearCreated <0) THEN
+	            RAISE EXCEPTION 'Years cannot be negative cannot be null';
+	        END IF;
+	    ENDIF;
+        RETURN NEW;
+    END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER check_year_listener BEFORE INSERT OR UPDATE ON Comedians
+    FOR EACH ROW EXECUTE PROCEDURE checkYear();
